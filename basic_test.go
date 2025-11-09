03bbfa86f934f3ef8554644f3514709e14b49ea8
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:50:59 -0800

    tests

--- basic_test.go
+++ basic_test.go
@@ -1,17 +1,16 @@
 package diff_test
 
 import (
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/diff"
 )
 
 func ExampleDiff_basic() {
 	// diff file1.txt file2.txt
 	// Note: This requires actual files to compare
-	yup.MustRun(
+	gloo.MustRun(
 		Diff("testdata/file1.txt", "testdata/file2.txt"),
 	)
 	// Output:
 	//
 }
-
--- unified_test.go
+++ unified_test.go
@@ -1,16 +1,15 @@
 package diff_test
 
 import (
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/diff"
 )
 
 func ExampleDiff_unified() {
 	// diff -u file1.txt file2.txt
-	yup.MustRun(
+	gloo.MustRun(
 		Diff("testdata/file1.txt", "testdata/file2.txt", Unified),
 	)
 	// Output:
 	//
 }
-
